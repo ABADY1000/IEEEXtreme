77483e587677729625e55ed6fbe4cc3fd6b953e5
ABADY1000 <abady1000@gmail.com>
2018-10-04 11:05:17 +0300

    Keep on track

--- RecursiveArray/Second.py
+++ RecursiveArray/Second.py
@@ -2,6 +2,23 @@
 # Abdullah 
 # Not a good solution
 
+from time import time
+
+begun = False
+start = 0
+end = 0
+
+def FindTime(name):
+
+    if begun:
+        end = time()
+        begun = False
+        print("{} time is: {}".format(name,end - start))
+    else:
+        start = time()
+        begun = True
+
+
 def ListGenerator(lis):
 
     newArray = []         
@@ -11,8 +28,12 @@ def ListGenerator(lis):
 
 
 if __name__ == '__main__':
-    size = int(input())
-    current = tuple([int(x) for x in input().split()])
+    
+    Data = open('D:\\programming\\Coders2030\\RecursiveArray\\Data.txt', mode='r')
+
+    # size = int(input())
+    size = int(Data.readline())
+    current = tuple([int(x) for x in Data.readline().strip().split()])
     output = 1
     data = [current]
     length = len(data)
@@ -25,6 +46,8 @@ if __name__ == '__main__':
     number_to_generate = 10
 
     while(True):
+
+        x = {12:15,6:8}
         for _ in range(number_to_generate):
             data.append(ListGenerator(data[-1]))
             if data[-1] == data[-2]:
